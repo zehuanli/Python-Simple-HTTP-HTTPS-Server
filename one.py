def1f018002c22eabd80a19417ad6368fc414ff6
Zehuan Li <22104836+zehuanli@users.noreply.github.com>
2020-01-10 10:57:14 -0800

    Update one.py

--- one.py
+++ one.py
@@ -1,8 +1,21 @@
-import BaseHTTPServer, ssl, os, time, sys, base64
+import BaseHTTPServer, ssl, os, time, argparse, base64, logging, re
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from threading import Thread
 
-global key
+
+# fail2ban: failregex = \[ERROR\] HTTPS? +<HOST>: *\d{1,5} - code \d{3}.*$
+file_formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(scheme)s %(message)s', datefmt='%m/%d/%y %H:%M:%S')
+console_formatter = logging.Formatter('\033[%(color)dm[%(scheme)-5s %(asctime)s] %(message)s\033[0m', datefmt='%m/%d/%y %H:%M:%S')
+file_handler = logging.FileHandler('/var/log/one.log', 'w', encoding='utf-8')
+file_handler.setFormatter(file_formatter)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(console_formatter)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+global key, cmd_args
 key = None
 
 class AuthHandler(SimpleHTTPRequestHandler, object):
@@ -26,19 +39,13 @@ class AuthHandler(SimpleHTTPRequestHandler, object):
     self.log(True, format, *args)
 
   def log(self, error, format, *args):
+    is_https = self.server.server_port == cmd_args.https_port
+    log_string = '%15s:%5d - %s'  % (self.client_address[0], self.client_address[1], format%args)
+    log_extra = {'color': 31 if error else (33 if is_https else 37), 'scheme': 'HTTPS' if is_https else 'HTTP'}
     if error:
-      sys.stderr.write('\n')
-    if self.server.server_port == 443:
-      sys.stderr.write('\033[%dm[HTTPS %s] %15s:%5d - %s\033[0m\n'  % (31 if error else 33, self.log_date_time_string(), self.client_address[0], self.client_address[1], format%args))
+      logger.error(log_string, extra=log_extra)
     else:
-      sys.stderr.write('\033[%dm[HTTP  %s] %15s:%5d - %s\033[0m\n'  % (31 if error else 37, self.log_date_time_string(), self.client_address[0], self.client_address[1], format%args))
-
-  def log_date_time_string(self):
-    now = time.time()
-    year, month, day, hh, mm, ss, x, y, z = time.localtime(now)
-    s = '%02d/%02d/%04d %02d:%02d:%02d' % (day, month, year, hh, mm, ss)
-    return s
-
+      logger.info(log_string, extra=log_extra)
 
 def start_server(daemon):
   if daemon:
@@ -46,21 +53,37 @@ def start_server(daemon):
 
 
 if __name__ == '__main__':
-  if len(sys.argv) >= 2:
-    # Usage: one.py [username:password]
-    key = base64.b64encode(sys.argv[1])
+  parser = argparse.ArgumentParser(description='Simple HTTP & HTTPS server in one.py')
+  parser.add_argument('--http', dest='http', action='store_true', help='enable HTTP, which is disabled by default')
+  parser.add_argument('--ip', dest='local_ip', type=str, help='local IP that listens for HTTP/HTTPS, default 0.0.0.0', default='0.0.0.0')
+  parser.add_argument('--http-port', dest='http_port', type=int, help='HTTP port, default 80', default=80)
+  parser.add_argument('--https-port', dest='https_port', type=int, help='HTTPS port, default 443', default=443)
+  parser.add_argument('-u', '--user', dest='username', help='username for HTTP basic authentication')
+  parser.add_argument('-p', '--pass', dest='password', help='password for HTTP basic authentication')
+  cmd_args = parser.parse_args()
+  if not re.match(r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$', cmd_args.local_ip):
+    parser.error('--ip should be a valid IPv4 address.')
+  if cmd_args.http_port != 80 and not cmd_args.http:
+    parser.error('--http-port specified while --http not set enabled.')
+  if (cmd_args.username is None) ^ (cmd_args.password is None):
+    parser.error('Either --user or --pass missing for HTTP basic authentication.')
+  if cmd_args.username:
+    key = base64.b64encode(cmd_args.username + ':' + cmd_args.password)
+
   server = BaseHTTPServer.HTTPServer
   handler = AuthHandler
-  http_daemon = server(('0.0.0.0', 80), handler)
-  https_daemon = server(('0.0.0.0', 443), handler)
+  if cmd_args.http:
+    http_daemon = server((cmd_args.local_ip, cmd_args.http_port), handler)
+  https_daemon = server((cmd_args.local_ip, cmd_args.https_port), handler)
   https_daemon.socket = ssl.wrap_socket(https_daemon.socket, certfile='/path/to/fullchain.pem', keyfile='/path/to/privkey.pem', server_side=True)
   os.chdir('www')
-  http_t = Thread(target=start_server, args=(http_daemon,))
-  http_t.daemon = True
+  if cmd_args.http:
+    http_t = Thread(target=start_server, args=(http_daemon,))
+    http_t.daemon = True
+    http_t.start()
   https_t = Thread(target=start_server, args=(https_daemon,))
   https_t.daemon = True
-  http_t.start()
   https_t.start()
-  print("server started")
+  logger.info('Server started, listening on ' + cmd_args.local_ip + ' port ' + str(cmd_args.https_port) + ' for HTTPS' + ((' and port ' + str(cmd_args.http_port) + ' for HTTP') if cmd_args.http else ''), extra={'color': 37, 'scheme': 'START'})
   while True:
     time.sleep(1)
